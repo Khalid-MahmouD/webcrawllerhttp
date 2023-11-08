929c0f984cebdb461d80d83b9f00d9c75badb96e
unknown <khaloooood827@gmail.com>
2023-11-08 18:32:46 +0300

    craw page recursive working

--- crawl.js
+++ crawl.js
@@ -1,62 +1,79 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL, currentURL, pages){
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+    if ( baseURLObj.hostname !== currentURLObj.hostname){
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    console.log(normalizedCurrentURL)
+    if ( pages[normalizedCurrentURL] > 0 ) {
+        pages[normalizedCurrentURL]++;
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
     console.log(`active crawling ${currentURL}`)
     try{
         const resp = await fetch(currentURL)
         if (resp.status > 399){
             console.log(`error in fetch with statusCode: ${resp.status}, on page ${currentURL}`)
-            return 
+            return pages
         }
         const contentType = resp.headers.get('content-type')
         if (!contentType.includes("text/html")) {
             console.log(`non html response, content type: ${contentType}, on page ${currentURL}`)
-            return 
+            return pages
         }
-        console.log(await resp.text());
-        }catch(err){
+        const htmlBody = await resp.text()
+        const nextURLs = await getURLsFromHTML(htmlBody, baseURL)
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+         
+    }catch(err){
         console.log(`error in fetch ${err.message}, on page ${currentURL}`)
+    }   
+    return pages
+}
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const aElements = dom.window.document.querySelectorAll('a')
+    for (const aElement of aElements){
+      if (aElement.href.slice(0,1) === '/'){
+        try {
+          urls.push(new URL(aElement.href, baseURL).href)
+        } catch (err){
+          console.log(`${err.message}: ${aElement.href}`)
+        }
+      } else {
+        try {
+          urls.push(new URL(aElement.href).href)
+        } catch (err){
+          console.log(`${err.message}: ${aElement.href}`)
+        }
+      }
     }
-    
-}
-
-function getUrlFromHTML(htmlBody, baseUrl) {
-    
-    const urls = [];
-    const dom = new JSDOM(htmlBody);
-    const linkELements = dom.window.document.querySelectorAll('a')
-    for (const linkElement of linkELements) {
-        if(linkElement.href.slice(0,1) === '/'){
-            //relative path
-            try{
-                const urlObj = new URL(`${baseUrl}${linkElement.href}`)
-                urls.push(urlObj.href)
-            }catch(err){
-                console.log(`error with relative path ${err.message}`)
-            }
-            
-        }else{
-            try{
-                const urlObj = new URL(linkElement.href)
-                urls.push(urlObj.href)
-            }catch(err){
-                console.log(`error with absolute path ${err.message}`)
-            }
-        }
-        }
     return urls
-}
-
-function normalizeURL(urlString){
-    const urlObj = new URL(urlString);
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    return hostPath.slice(-1) === '/' ?  hostPath.slice(0, -1) : hostPath
-}
+  }
+  
+  function normalizeURL(url){
+    const urlObj = new URL(url)
+    let fullPath = `${urlObj.host}${urlObj.pathname}`
+    if (fullPath.length > 0 && fullPath.slice(-1) === '/'){
+      fullPath = fullPath.slice(0, -1)
+    }
+    return fullPath
+  }
 
 
 module.exports = {
-    normalizeURL: normalizeURL,
-    getUrlFromHTML: getUrlFromHTML,
-    crawlPage:crawlPage
+     normalizeURL,
+     getURLsFromHTML,
+     crawlPage
     
 }
--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-const { crawlPage } =  require('./crawl.js')
-function main(){
-    if (process.argv.length < 3){
-        console.log('no website provided')
-        process.exit(1)
-    }
-    if (process.argv.length > 3){
-        console.log('Too many arguments')
-        process.exit(1)
-    }
-    const baseUrl = process.argv[2]
-    console.log(`Starting crawl of ${baseUrl}`)
-    crawlPage(baseUrl)
+const { crawlPage } = require('./crawl.js')
+
+async function main(){
+  if (process.argv.length < 3){
+    console.log('no website provided')
+  }
+  if (process.argv.length > 3){
+    console.log('too many arguments provided')
+  }
+
+  const baseURL = process.argv[2]
+
+  console.log(`starting crawl of: ${baseURL}...`)
+
+  const pages = await crawlPage(baseURL, baseURL, {})
+
+  console.log(pages)
 }
 
 main()
